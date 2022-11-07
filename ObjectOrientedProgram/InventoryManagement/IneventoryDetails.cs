ab4692199250fe1b11610026fa4656dcd10acdfb
PriyankaRumal <priyankarumal182000@gmail.com>
2022-11-06 20:21:13 -0800

    uc1 added

---
+++ ObjectOrientedProgram/InventoryManagement/IneventoryDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedProgram.InventoryManagement
+{
+    public class IneventoryDetails
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+    }
+}
